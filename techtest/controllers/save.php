15f83a7dc00c65f710da24fda41d3709bec04781
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-06-13 10:32:32 +0000

    git-svn-id: http://svn.turbocrms.com/code/techtests/skybet@1149 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- techtest/controllers/save.php
+++ techtest/controllers/save.php
@@ -16,8 +16,10 @@ foreach($_POST['firstname'] as $key => $firstname){
 	}
 }
 people::get_instance()
-->set_people($new_people)
-->save();
+	->set_people($new_people)
+	->save();
 if(!isset($_GET['ajax'])){
-header("Location: index.php");
+	header("Location: index.php");
+}else{
+	
 }
