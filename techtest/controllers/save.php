49c7e6a5c284b44a0fe0e7eb63226b306cbe806d
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-06-13 09:59:05 +0000

    git-svn-id: http://svn.turbocrms.com/code/techtests/skybet@1147 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

---
+++ techtest/controllers/save.php
@@ -0,0 +1,22 @@
+<?php 
+// Clear out the currently stored people.
+people::get_instance()->clear_people();
+
+// Load the post data into person objects into an array
+$new_people = array();
+foreach($_POST['firstname'] as $key => $firstname){
+	$surname = $_POST['lastname'][$key];
+	$job = $_POST['job'][$key];
+	$new_person = person::factory()
+	->setFirstname($firstname)
+	->setSurname($surname)
+	->setJobTitle($job);
+	if($new_person->isValid()){
+		$new_people[] = $new_person;
+	}
+}
+people::get_instance()
+->set_people($new_people)
+->save();
+
+header("Location: index.php");
